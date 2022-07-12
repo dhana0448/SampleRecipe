499bf3a0bcc7b503b38a885fab5a7e2a1cc1e414
Sykam <dhana-lakshmi.sykam@capgemini.com>
2022-07-12 17:27:01 +0530

    Sample Recpie

--- src/main/java/com/samplerecipe/service/RecipeServiceImpl.java
+++ src/main/java/com/samplerecipe/service/RecipeServiceImpl.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-
+import com.samplerecipe.controller.RecipeController;
 import com.samplerecipe.entity.RecipeEntity;
 import com.samplerecipe.exceptions.IdNotFoundException;
 import com.samplerecipe.exceptions.NoDataException;
@@ -23,19 +25,24 @@ public class RecipeServiceImpl implements RecipeServiceInterface {
 	 @Autowired
 	  RecipeRepository rs;
 
-	
+	 Logger logger= LoggerFactory.getLogger(RecipeController.class);
 	 @Override
 	public List<RecipeEntity> getallrecipes() throws NoRecipeFoundException{
 		 List<RecipeEntity> m= rs.findAll();
 		 if (m.isEmpty()) {
+			 logger.error("No recipes found in database");
 			 throw new NoRecipeFoundException("No Recipes Found");
 		 }
+		 int i;
+         logger.warn("i is not used in anywhere");
+     logger.info("Fetch all Recipes from the database");
 		return rs.findAll();
 	}
 
 	 
 	@Override
 	public RecipeEntity addRecipes(RecipeEntity r) {
+		logger.info("Successfully created the recipe");
 		return rs.save(r);
 	}
 
@@ -44,9 +51,11 @@ public class RecipeServiceImpl implements RecipeServiceInterface {
 	public String deleteRecipe(int rId) throws NoDataException {
 		Optional<RecipeEntity> m= rs.findById(rId);
 		if(!m.isPresent()) {
+			logger.error("There is no recipe with the Id {} to delete",rId);
 			throw new NoDataException("Oops we can't find this Id!!");
 		}
 		rs.deleteById(rId);
+		logger.info("Successfully deleted the recipe with Id {}",rId);
 		return "Removed";
 	   }
 
@@ -55,6 +64,7 @@ public class RecipeServiceImpl implements RecipeServiceInterface {
 	public RecipeEntity updateRecipe(int rId,RecipeEntity r) {	
 			RecipeEntity m=  rs.findById(rId).get();
 				  if(m!=null) { 
+					  logger.error("Recipe with the Id {} is not in database to update",rId);
 				    m.setRecipeName(r.getRecipeName());
 				    m.setCategory(r.getCategory());
 				    m.setRecipeType(r.getRecipeType());
@@ -70,8 +80,10 @@ public class RecipeServiceImpl implements RecipeServiceInterface {
 	public RecipeEntity getRecipesbyId(int rId) throws IdNotFoundException {
         Optional<RecipeEntity> m= rs.findById(rId);
 	    if(!m.isPresent()) {
+	    	logger.error("recipe with the Id {} is not in database",rId);
 		   throw new IdNotFoundException("Sorry Recipe Id not found!!");
 	      }
+	    logger.info("fetch all details of the recipe with the Id of {}",rId);
 	   return m.get();
 	}
 
